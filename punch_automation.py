6a21f9fd69219a03f3e9fb042ec2ed7d737684d3
Mark Mon Monteros <monteros.markmon@gmail.com>
2022-12-30 15:36:37 +0800

    Update punch_automation.py

--- punch_automation.py
+++ punch_automation.py
@@ -2,7 +2,6 @@ __author__ = 'Mark Mon Monteros'
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -44,6 +43,13 @@ class PunchAutomation():
 			if self.current_time == self.start_shift:
 				if self.holiday_check() != 'undefined':
 					print('\nToday is a Holiday: ' + self.holiday.text)
+					if self.leave_check() != 'undefined':
+						print('\nStatus: ' + self.leave.text)
+						self.exit()
+					else:
+						self.punch_in()
+				elif self.leave_check() != 'undefined':
+					print('\nStatus: ' + self.leave.text)
 					self.exit()
 				else:
 					self.punch_in()
@@ -63,6 +69,13 @@ class PunchAutomation():
 		except:
 			return 'undefined'
 
+	def leave_check(self):
+		self.browser.get(self.website + '/view-shift-schedule')
+		try:
+			self.leave = WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='v2-main']//div[@class='v2-content']//div[@class='row']//div[@class='col-12 content-viewshift']//div[@class='main-viewshift']//div[@class='card']//div[@class='card-content']//div[@class='calendar']//div[@class='calendar-days']//div[@class='calendar-item is-today is-active']//div[@id='on-leave-display']//div")))
+		except:
+			return 'undefined'
+
 	def punch_in(self):
 		try:
 			self.clock_in = WebDriverWait(self.browser,5).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='button button-clockin']")))
@@ -74,7 +87,7 @@ class PunchAutomation():
 
 	def punch_out(self):
 		try:
-			self.clock_out = WebDriverWait(self.browser,3).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='button button-clockout']")))
+			self.clock_out = WebDriverWait(self.browser,5).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='button button-clockout']")))
 			self.clock_out.click()
 			print("\n[*] - CLOCKING OUT @ " + self.current_time)
 		except:
